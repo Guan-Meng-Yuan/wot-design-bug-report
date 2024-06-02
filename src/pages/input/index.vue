ee6f116320fac2a85316657a1e948d574e61bfa4
关梦园 <guan.mengyuan@icloud.com>
2024-06-03 00:29:50 +0800

    添加原生组件

--- src/pages/input/index.vue
+++ src/pages/input/index.vue
@@ -1,8 +1,16 @@
 <template>
   <div>digit值:{{ digitValue }}</div>
   <wd-input label="digit类型" v-model="digitValue" type="digit"></wd-input>
+  <div>
+    <div mr-10rpx>原生组件digit</div>
+    <input type="digit" v-model="digitValue">
+  </div>
   <div>number值:{{ numberValue }}</div>
   <wd-input label="number类型" v-model="numberValue" type="number"></wd-input>
+  <div>
+    <div mr-10rpx>原生组件number</div>
+    <input type="digit" v-model="numberValue">
+  </div>
 </template>
 
 <script setup lang="ts">
